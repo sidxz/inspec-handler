e91650859356595467bbfe9cb236197c73acc816
Siddhant Rath <sid@tamu.edu>
2017-04-20 17:05:08 -0500

    Feature log branch

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
-default['inspec_handler']['run_path'] = "/etc/chef/inspec-handler"
+default['inspec_handler']['run_path'] = "/opt/coe/inspec-tests"
 default['inspec_handler']['log_path'] = "/var/log/inspec_handler" 
-default['inspec_handler']['enforced'] = true
+default['inspec_handler']['enforced'] = false
 default['inspec_handler']['whitelist']= nil
 default['inspec_handler']['blacklist']= nil
 default['inspec_handler']['environment']= nil
