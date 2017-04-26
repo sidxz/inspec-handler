354cc51416b9ca4499e01773bd225bca9cf2f30d
Siddhant Rath <sid@tamu.edu>
2017-04-26 14:15:32 -0500

    Bug Fixes

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sid@tamu.edu'
 license 'all_rights'
 description 'Installs/Configures inspec_handler'
 long_description 'Installs/Configures inspec_handler'
-version '1.0.1'
+version '1.0.2'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
